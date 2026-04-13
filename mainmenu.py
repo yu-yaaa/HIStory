2b92d34168c3f862fc2d2232acef532a6329c192
Yu <yuschool101@gmail.com>
2026-04-13 17:44:48 +0800

    logo pro maxing

--- mainmenu.py
+++ mainmenu.py
@@ -2,20 +2,41 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800,600),pygame.RESIZABLE)
-pygame.display.set_caption('HIStory')
+
+screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 clock = pygame.time.Clock()
-logo = pygame.image.load('Assets/HIStory Logo.png').convert_alpha()
 
-mainmenubg = pygame.image.load('Assets/Main Menu background.png').convert_alpha()
+logo = pygame.image.load("Assets/HIStory Logo.png").convert_alpha()
+bg = pygame.image.load("Assets/Main Menu background.png").convert()
 
-while True:
+def scale_assets(width, height):
+    bg_scaled = pygame.transform.scale(bg, (width, height))
+
+    logo_width = int(width * 0.3)
+    logo_height = int(logo_width * (logo.get_height() / logo.get_width()))
+    logo_scaled = pygame.transform.smoothscale(logo, (logo_width, logo_height))
+
+    return bg_scaled, logo_scaled
+
+bg_scaled, logo_scaled = scale_assets(800, 600)
+
+running = True
+
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
+            running = False
 
-    screen.blit(mainmenubg,(0,0))
-    screen.blit(logo,(10,10))
+        if event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
+            bg_scaled, logo_scaled = scale_assets(event.w, event.h)
+
+    screen.blit(bg_scaled, (0, 0))
+    screen.blit(logo_scaled, (30, 30))
 
     pygame.display.update()
     clock.tick(60)
+
+pygame.quit()
+exit()
