fabbca129048f17af7a0904439718b8102e51710
DyingFesh <melissa1220schoolacc@gmail.com>
2026-04-26 15:42:06 +0800

    add comments

--- login.py
+++ login.py
@@ -1,19 +1,19 @@
 import pygame
 from text_field import TextInput
 from button_class import Button
-from login_register_base import *
+from login_register_base import * # This import the base for login and resgister page
 
-def run_login_display(login_box, login_box_rect):
-    screen.blit(bg_img, (0, 0))
-    screen.blit(login_box, login_box_rect)
-    draw_text("Log In Page", 
+def run_login_display(login_box, login_box_rect): # This is to draw all of the display
+    screen.blit(bg_img, (0, 0)) #background 
+    screen.blit(login_box, login_box_rect) # semi-transparent white box for fields
+    draw_text("Log In Page",    # Title
               login_box_rect.centerx, 
               login_box_rect.top + int(box_height * 0.06), 
               black, 
               size=72, 
               anchor="center")
-    screen.blit(logo_img, logo_img_rect)
-    screen.blit(exit_img, exit_rect)
+    screen.blit(logo_img, logo_img_rect) # call logo
+    screen.blit(exit_img, exit_rect) # exit icon 
     draw_text("User Name", 
               login_box_rect.left + 30, 
               login_box_rect.top + int(box_height * 0.2), 
@@ -22,28 +22,28 @@ def run_login_display(login_box, login_box_rect):
               login_box_rect.left + 30, 
               login_box_rect.top + int(box_height * 0.43), 
               size = int(screen_height * 0.04))
-    username_field.draw(screen)
+    username_field.draw(screen) 
     password_field.draw(screen)
     confirm_button.draw(screen)
     sign_up_button.draw(screen)
     show_password_button.draw(screen)
     
-login_box, login_box_rect, box_height, box_width = draw_white_box()
+login_box, login_box_rect, box_height, box_width = draw_white_box() # create white box
 
-username_field = TextInput(login_box_rect.left + 30, 
+username_field = TextInput(login_box_rect.left + 30, # create username field for login
                            login_box_rect.top + int(box_height * 0.3), 
                            int(box_width * 0.9) , 
                            60, 
                            font_size = int(screen_height * 0.035) )
 
-password_field = TextInput(login_box_rect.left + 30, 
+password_field = TextInput(login_box_rect.left + 30,  # create password field for login
                            login_box_rect.top + int(box_height * 0.54), 
                            int(box_width * 0.7), 
                            60,
                            font_size = int(screen_height * 0.035), 
-                           is_password = True)
+                           is_password = True) # is_password for hiding password
 
-confirm_button = Button("Confirm",
+confirm_button = Button("Confirm",  # create comfirm button to login
                         login_box_rect.centerx - int(box_width * 0.45) // 2,
                         login_box_rect.top + int(box_height * 0.75),
                         int(box_width * 0.45), 
@@ -60,7 +60,7 @@ btn_w   = int(screen_width  * 0.25)   # button width
 btn_h   = int(screen_height * 0.07)   # button height
 padding = 30                           # gap from screen edge
 
-sign_up_button = Button( "Sign up for an account here", 
+sign_up_button = Button( "Sign up for an account here", # create sign up button to direct user to register page
                         screen_width  - btn_w - padding, 
                         screen_height - btn_h - padding, 
                         btn_w, 
@@ -73,7 +73,7 @@ sign_up_button = Button( "Sign up for an account here",
                         50, 
                         white)
 
-show_password_button = Button("SHOW", 
+show_password_button = Button("SHOW",   # create button for users to show what user type in password field
                     login_box_rect.left + int(box_width * 0.75),
                     login_box_rect.top + int(box_height * 0.56), 
                     int(box_width * 0.22),
@@ -90,17 +90,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN: # for when exit icon is clicked user will quite the game
             if exit_rect.collidepoint(event.pos):
                 running = False
 
         username_field.handle_event(event)
         password_field.handle_event(event)
-        if show_password_button.is_clicked(event):
+        if show_password_button.is_clicked(event):  #  when show_password_button is clicked password will be shown and change hide and show word
             password_field.toggle_visibility()
             show_password_button.text = "HIDE" if not password_field.is_hidden else "SHOW"
         
-    run_login_display(login_box, login_box_rect)
+    run_login_display(login_box, login_box_rect) # call function for display everything
     pygame.display.flip()
     clock.tick(60)
 
--- register.py
+++ register.py
@@ -1,12 +1,12 @@
 import pygame
-from text_field import TextInput
-from button_class import Button
-from login_register_base import *
+from text_field import TextInput    # import text field for user input
+from button_class import Button # Import button class to create button
+from login_register_base import *   # This import the base for login and resgister page
 
-logo_img_rect.center = (screen_width // 2, screen_height // 8.5)
-login_box, login_box_rect, box_height, box_width = draw_white_box(1.5)
+logo_img_rect.center = (screen_width // 2, screen_height // 8.5)    #customise HIStory logo placement
+login_box, login_box_rect, box_height, box_width = draw_white_box(1.5)  # customise semi-transparent white box height for register page
 
-def run_register_display(login_box, login_box_rect):
+def run_register_display(login_box, login_box_rect):    # function to draw everything needed for register page
     screen.blit(bg_img, (0, 0))
     screen.blit(login_box, login_box_rect)
     draw_text("Register Page", 
@@ -48,33 +48,33 @@ def run_register_display(login_box, login_box_rect):
     comfirm_button.draw(screen)
     login_button.draw(screen)
     
-gmail_field = TextInput(x = login_box_rect.left + 30, 
+gmail_field = TextInput(x = login_box_rect.left + 30,  # gmail text field
                            y = login_box_rect.top + int(box_height * 0.215),
                            width = int(box_width - 60),
                            height = int(box_height * 0.06),
                            font_size = int(screen_height * 0.035))
 
-user_field = TextInput(x = login_box_rect.left + 30, 
+user_field = TextInput(x = login_box_rect.left + 30,    # username text field
                            y = login_box_rect.top + int(box_height * 0.365),
                            width = int(box_width - 60),
                            height = int(box_height * 0.06),
                            font_size = int(screen_height * 0.035))
 
-password_field = TextInput(x = login_box_rect.left + 30, 
+password_field = TextInput(x = login_box_rect.left + 30,    # password text field
                            y = login_box_rect.top + int(box_height * 0.515),
                            width = int(box_width - 80 - (box_width * 0.22)),
                            height = int(box_height * 0.06),
                            font_size = int(screen_height * 0.035),
                            is_password = True)
 
-comfirm_password_field = TextInput(x = login_box_rect.left + 30, 
+comfirm_password_field = TextInput(x = login_box_rect.left + 30,    # comfirm password field
                            y = login_box_rect.top + int(box_height * 0.665),
                            width = int(box_width - 80 - (box_width * 0.22)),
                            height = int(box_height * 0.06),
                            font_size = int(screen_height * 0.035),
                            is_password = True)
 
-student_role_button = Button("Student",
+student_role_button = Button("Student", # student role button
                              x = login_box_rect.left + 30,
                              y = login_box_rect.top + int(box_height * 0.82),
                              w = int(box_width * 0.45),
@@ -85,10 +85,9 @@ student_role_button = Button("Student",
                              border_r = 15,
                              border_w = 2,
                              font_size = int(screen_height * 0.035),
-                             font_color = black
-                             )
+                             font_color = black)
 
-teacher_role_button = Button("Teacher",
+teacher_role_button = Button("Teacher", # teacher role button
                              x = login_box_rect.right - int(box_width * 0.45) - 30,
                              y = login_box_rect.top + int(box_height * 0.82),
                              w = int(box_width * 0.45),
@@ -99,10 +98,9 @@ teacher_role_button = Button("Teacher",
                              border_r = 15,
                              border_w = 2,
                              font_size = int(screen_height * 0.035),
-                             font_color = black
-                             )
+                             font_color = black)
 
-show_password_button = Button("SHOW", 
+show_password_button = Button("SHOW",   # show password button
                     x =login_box_rect.left + int(box_width * 0.75),
                     y = login_box_rect.top + int(box_height * 0.515), 
                     w = int(box_width * 0.22),
@@ -115,7 +113,7 @@ show_password_button = Button("SHOW",
                     font_size = int(screen_height * 0.03),
                     font_color = white)
 
-show_comfirm_password_button = Button("SHOW", 
+show_comfirm_password_button = Button("SHOW",   # show comfirm password
                     x =login_box_rect.left + int(box_width * 0.75),
                     y = login_box_rect.top + int(box_height * 0.665), 
                     w = int(box_width * 0.22),
@@ -128,7 +126,7 @@ show_comfirm_password_button = Button("SHOW",
                     font_size = int(screen_height * 0.03),
                     font_color = white)
 
-comfirm_button = Button("Confirm", 
+comfirm_button = Button("Confirm",  # comfirm button for sign up
                     x = login_box_rect.centerx - int(box_width * 0.45) // 2,
                     y = login_box_rect.top + int(box_height * 0.91), 
                     w = int(box_width * 0.4),
@@ -141,7 +139,7 @@ comfirm_button = Button("Confirm",
                     font_size = int(screen_height * 0.04),
                     font_color = white)
 
-login_button = Button("Have an account? Log In Here", 
+login_button = Button("Have an account? Log In Here",   # login button for users that have an account to login
                     x = screen_width - int((screen_width * 0.27) + 30),
                     y = screen_height - int((screen_height * 0.05) + 30), 
                     w = int(screen_width * 0.27),
