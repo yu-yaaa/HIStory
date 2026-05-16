e092acda68a6aa6146095f4d19564c03f40f2b40
DyingFesh <melissa1220schoolacc@gmail.com>
2026-05-17 03:15:15 +0800

    connect teacher to main (i hate python terminal sm)

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from register import run_register
 from student_profile import run_student_profile
 from user_profile import ProfilePicture
 from studentmainmenu import run_student_mainmenu
+from stud_management import draw_stud_manage
+from teacher_dashboard import draw_dashboard
+from teacher_profile import run_teacher_profile
 import session
 
 profile_pic = None
@@ -18,12 +21,18 @@ current_scene = "login" # User is automatically send to the login page when the 
 running = True
 show_join_popup = False 
 show_change_pw_popup =False
+show_tc_change_pw_popup = False
 while running:
     events = pygame.event.get()
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_click = False
 
     for event in events:
+        
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_click = True
 
     if current_scene == "login":    # handles navigation buttons on login page
         result = run_login(events)
@@ -33,6 +42,8 @@ while running:
             current_scene = "register"
         elif result == "student_menu":
             current_scene = "student_menu"
+        elif result == "dashboard":
+            current_scene = "dashboard"
             
     elif current_scene == "register":   # handles navigation buttons on register page
         result = run_register(events)
@@ -55,7 +66,30 @@ while running:
             box_y    = int(screen.get_height() * 0.07),   # ← higher up
             box_size = int(screen.get_height() * 0.2))
         current_scene, show_join_popup, show_change_pw_popup = run_student_profile(events, show_join_popup, profile_pic, show_change_pw_popup)
-    # reset when leaving profile page
+    
+    elif current_scene == "dashboard":
+        action = draw_dashboard(screen, events)
+
+        if action == "exit":
+            running = False
+
+        elif action == "manage_students":
+            current_scene = "manage_students"
+        
+        elif action == "teacher_profile":
+            current_scene = "teacher_profile"
+            
+    elif current_scene == "manage_students":
+        action = draw_stud_manage(screen, events)
+
+        if action == "dashboard":
+            current_scene = "dashboard" 
+
+    elif current_scene == "teacher_profile":
+        result, show_tc_change_pw_popup = run_teacher_profile(events, show_change_pw_popup)
+        if result == "manage_students":
+            current_scene = "manage_students"
+    # reset when leaving profile page        
     if current_scene != "profile":
         profile_pic = None
                 
